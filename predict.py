d72c9ab22fb74e35b65ed124357a7596962527af
lzl32947@gmail.com <githublzl980223>
2020-03-07 15:43:35 +0800

    Change the default rule of prediction.

--- predict.py
+++ predict.py
@@ -6,6 +6,7 @@ import keras.backend as K
 import matplotlib.pyplot as plt
 from PIL import Image
 from networks.ssd_linked import get_SSD_model
+from parameter.parameters import DataParameter
 from util.decode_util import *
 from util.drawing_util import draw_image
 from util.img_process_util import process_single_input
@@ -16,20 +17,14 @@ if __name__ == '__main__':
     sess = tf.Session(config=config)
     K.set_session(sess)
     model = get_SSD_model()
-    model.load_weights("./checkpoints/ep005-loss0.741-val_loss0.680.h5", by_name=True)
-    for root, dirs, files in os.walk('g_data'):
+    model.load_weights("./checkpoints/ep020-loss0.352-val_loss0.147.h5", by_name=True)
+    for root, dirs, files in os.walk(r'G:\data_stored\test_line'):
         for file in files:
             img_path = os.path.join(root, file)
-            # img_path = "./img/street2.jpg.jpg"
             image = Image.open(img_path)
-            image = image.resize((300, 300), Image.ANTIALIAS)
+            if DataParameter.reshape_only:
+                image = image.resize((300, 300), Image.ANTIALIAS)
             photo, x_offset_ratio, y_offset_ratio, img = process_single_input(image)
-            # k = img.resize((300, 300), Image.ANTIALIAS)
-            # photo = np.reshape(np.array(k), (1, HyperParameter.min_dim, HyperParameter.min_dim, 3))
-            # plt.figure()
-            # plt.imshow(photo)
-            # plt.show()
-            # plt.close()
             photo = photo.reshape((1, HyperParameter.min_dim, HyperParameter.min_dim, 3))
             result_list = model.predict(photo)
             result_decode = decode_predict(result_list)
