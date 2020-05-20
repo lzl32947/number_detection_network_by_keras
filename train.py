d2c08f26e369dbae6ffdc28556cebb0d5b44f8fc
LiuZhilin <lzl32947@163.com>
2020-05-20 14:39:46 +0800

    Update train.py by adding comments.

--- train.py
+++ train.py
@@ -15,53 +15,53 @@ from util.input_util import data_generator, get_prior_box_list, get_weight_file
 
 
 def loss_function(y_t, y_p):
+    """
+    Custom loss function for SSD
+    :param y_t: tf.placeholder, the y_true
+    :param y_p: tf.placeholder, the y_pred
+    :return: the loss of the batch
+    """
     def _l1_smooth_loss(y_true, y_pred):
+        """
+        L1 smooth loss
+        :param y_true: tf.placeholder, the y_true
+        :param y_pred: tf.placeholder, the y_pred
+        :return: the loss of the location loss adapting l1-smooth
+        """
         abs_loss = tf.abs(y_true - y_pred)
         sq_loss = 0.5 * (y_true - y_pred) ** 2
         l1_loss = tf.where(tf.less(abs_loss, 1.0), sq_loss, abs_loss - 0.5)
         return tf.reduce_sum(l1_loss, -1)
 
     def _softmax_loss(y_true, y_pred):
+        """
+        Classification loss
+        :param y_true:  tf.placeholder, the y_true
+        :param y_pred:  tf.placeholder, the y_pred
+        :return: the loss of the classification
+        """
         y_pred = tf.maximum(tf.minimum(y_pred, 1 - 1e-15), 1e-15)
         softmax_loss = -tf.reduce_sum(y_true * tf.log(y_pred), axis=-1)
         return softmax_loss
 
     batch_size = tf.shape(y_t)[0]
     num_boxes = tf.to_float(tf.shape(y_t)[1])
-    # 计算所有的loss
-    # 分类的loss
-    # batch_size,8732,21 -> batch_size,8732
     conf_loss = _softmax_loss(y_t[:, :, 4:-8], y_p[:, :, 4:-8])
-    # 框的位置的loss
-    # batch_size,8732,4 -> batch_size,8732
     loc_loss = _l1_smooth_loss(y_t[:, :, :4], y_p[:, :, :4])
-    # 获取所有的正标签的loss
-    # 每一张图的pos的个数
     num_pos = tf.reduce_sum(y_t[:, :, -8], axis=-1)
-    # 每一张图的pos_loc_loss
     pos_loc_loss = tf.reduce_sum(loc_loss * y_t[:, :, -8], axis=1)
-    # 每一张图的pos_conf_loss
     pos_conf_loss = tf.reduce_sum(conf_loss * y_t[:, :, -8], axis=1)
-    # 获取一定的负样本
     num_neg = tf.minimum(Config.neg_pos_ratio * num_pos, num_boxes - num_pos)
-    # 找到了哪些值是大于0的
     pos_num_neg_mask = tf.greater(num_neg, 0)
-    # 获得一个1.0
     has_min = tf.to_float(tf.reduce_any(pos_num_neg_mask))
     num_neg = tf.concat(axis=0, values=[num_neg, [(1 - has_min) * Config.negatives_for_hard]])
-    # 求平均每个图片要取多少个负样本
     num_neg_batch = tf.reduce_mean(tf.boolean_mask(num_neg, tf.greater(num_neg, 0)))
     num_neg_batch = tf.to_int32(num_neg_batch)
-    # conf的起始
     confs_start = 4 + Config.bg_class + 1
-    # conf的结束
     confs_end = confs_start + len(Config.class_names)
-    # 找到实际上在该位置不应该有预测结果的框，求他们最大的置信度。
     max_confs = tf.reduce_max(y_p[:, :, confs_start:confs_end],
                               axis=2)
-    # 取top_k个置信度，作为负样本
     _, indices = tf.nn.top_k(max_confs * (1 - y_t[:, :, -8]), k=num_neg_batch)
-    # 找到其在1维上的索引
     batch_idx = tf.expand_dims(tf.range(0, batch_size), 1)
     batch_idx = tf.tile(batch_idx, (1, num_neg_batch))
     full_indices = (tf.reshape(batch_idx, [-1]) * tf.to_int32(num_boxes) +
@@ -69,7 +69,6 @@ def loss_function(y_t, y_p):
     neg_conf_loss = tf.gather(tf.reshape(conf_loss, [-1]), full_indices)
     neg_conf_loss = tf.reshape(neg_conf_loss, [batch_size, num_neg_batch])
     neg_conf_loss = tf.reduce_sum(neg_conf_loss, axis=1)
-    # 求loss总和
     total_loss = pos_conf_loss + neg_conf_loss
     total_loss /= (num_pos + tf.to_float(num_neg_batch))
     num_pos = tf.where(tf.not_equal(num_pos, 0), num_pos, tf.ones_like(num_pos))
@@ -90,6 +89,14 @@ def init_session():
 
 
 def get_model(weight_file, load_by_name, model_name, show_image=False):
+    """
+    The function that get the model from the 'model' package.
+    :param weight_file: str, the path to weight file if needed
+    :param load_by_name: bool, whether to load the weight file using 'by_mane'
+    :param model_name: ModelConfig object
+    :param show_image: bool, whether show the model structure
+    :return: the selected model
+    """
     prior_box_list = get_prior_box_list(model_name)
     if model_name == ModelConfig.VGG16:
         model = VGG16_SSD(prior_box_list)
@@ -113,6 +120,17 @@ def get_model(weight_file, load_by_name, model_name, show_image=False):
 
 def train(weight_file_list, load_weight_by_name, model_name, use_generator=False, show_image=False, show_summary=False,
           batch_size=4):
+    """
+    Train method, train SSD model.
+    :param weight_file_list: list, the list of weight file path
+    :param load_weight_by_name: list, the list of boolean
+    :param model_name: ModelConfig object
+    :param use_generator: bool, whether to use the image generator, if true, the model will train only on generated images
+    :param show_image: bool, whether to show the image of model
+    :param show_summary: bool, whether to show the model summary
+    :param batch_size: int, the size of one batch
+    :return: None
+    """
     init_session()
     model = get_model(weight_file=weight_file_list, show_image=show_image, model_name=model_name,
                       load_by_name=load_weight_by_name)
@@ -143,11 +161,11 @@ def train(weight_file_list, load_weight_by_name, model_name, use_generator=False
                        prior_box_list=prior_box_list, model_name=model_name,
                        use_generator=use_generator, batch_size=batch_size),
         steps_per_epoch=1000,
-        epochs=500,
+        epochs=50,
         validation_data=data_generator(Config.valid_annotation_path, method=PMethod.Reshape,
                                        prior_box_list=prior_box_list, model_name=model_name,
                                        use_generator=use_generator, batch_size=batch_size),
-        validation_steps=100,
+        validation_steps=250,
         initial_epoch=0,
         callbacks=[logging, checkpoint],
         verbose=1)
@@ -155,10 +173,10 @@ def train(weight_file_list, load_weight_by_name, model_name, use_generator=False
 
 if __name__ == '__main__':
     train(weight_file_list=[get_weight_file("ssd_weights.h5"),
-                            get_weight_file("resnet101_weights_tf_dim_ordering_tf_kernels_notop.h5")],
+                            get_weight_file("vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5")],
           load_weight_by_name=[True, True],
-          model_name=ModelConfig.ResNet101,
+          model_name=ModelConfig.VGG16,
           show_image=True,
           batch_size=2,
-          use_generator=True,
+          use_generator=False,
           show_summary=True)
